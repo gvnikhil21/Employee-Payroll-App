8e5b21d04d9dee9537a8301bd8dde9b3334e6a4d
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-11-26 17:32:55 +0530

    Added: Code added to retrieve employee pyaroll data from JSON server

---
+++ js/siteProperties.js
@@ -0,0 +1,6 @@
+const site_properties = {
+    local_storage: "true",
+    home_page: "../pages/home.html",
+    payroll_page: "../pages/employeePayRoll.html",
+    server_url: "http://127.0.0.1:3000/EmployeePayRollDB"
+};
